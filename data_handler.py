9499812cf7f7ceb49bcbdecfd9439dc7b1ade9cf
Montague <6136789+MontagueM@users.noreply.github.com>
2021-03-03 16:50:32 +0000

    Added some handling to produce features and labels

--- data_handler.py
+++ data_handler.py
@@ -13,4 +13,22 @@ Dataset heart.csv will only be used temporarily, attributes:
     David W. Aha (aha '@' ics.uci.edu) (714) 856-8779
 """
 
-df = pd.read_csv('heart.csv')
+
+def get_data():
+    df = pd.read_csv('heart.csv')
+    desired_cols = [
+        'age',
+        'sex',
+        'trestbps',
+        'target'
+    ]
+    df = df[df.columns.intersection(desired_cols)]
+    # Forming a feature/label array for ease of use with svm. Code is not nice but basically just converts to two arrays of the relevant features and labels needed for SVM classification
+    features = [df[x].tolist() for x in desired_cols[:-1]]
+    feat = [[features[i][j] for i in range(len(features))] for j in range(len(features[0]))]
+    labels = df[desired_cols[-1]].tolist()
+    return feat, labels
+
+
+if __name__ == '__main__':
+    features, labels = get_data()
