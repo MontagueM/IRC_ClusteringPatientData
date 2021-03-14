bcda0e00c98c4fc67de7d654bc7702b241a572b6
Amin Akhtar <aa7019@ic.ac.uk>
2021-03-14 01:00:43 +0000

    Added model evaluation-confusion matrix,

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import data_handler
 import model
 import graph
 import numpy as np
+from sklearn.metrics import plot_roc_curve
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     feature_names, features, labels = data_handler.get_data()
 
-    # Uncomment if visualising data
+    #Uncomment if visualising data
     #features = [x[:2] for x in features]
     #feature_names = feature_names[:2]
 
@@ -16,14 +18,23 @@ if __name__ == '__main__':
     predictions = model.test_data(svm_clf, test_features)
     accuracy = model.get_accuracy(test_labels, predictions)
 
+    print(f'Model accuracy: {round(accuracy*100, 1)}%')
+
+    #uncomment for confusion matrix and accuracy 
+    #model.evaluation(test_labels, predictions)
+
+
+    #uncomment for ROC curve and AUC score
+    #svc_disp = plot_roc_curve(svm_clf, test_features, test_labels)
+    #plt.show()
+   
     #uncomment to get 5 cross validation scores
     #scores = data_handler.k_fold_cross_validation(k=5, kernel= 'linear',features=features,labels=labels)
     #print(scores)
-    
-    #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    #(I'm not completely sure if the features and labels are used correctly in this implementation but
-    #the output seems ok. Still am unsure of the interpretation of the scores.)
+    cv = 
 
-    print(f'Model accuracy: {round(accuracy*100, 1)}%')
 
+    #model.auc_plot(test_features, test_labels)
+
+    #uncomment for 2D scatter plot
     #graph.visualise_clf(np.array(features), np.array(labels), svm_clf, feature_names)
--- model.py
+++ model.py
@@ -1,4 +1,8 @@
 from sklearn import svm, metrics
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import roc_curve, roc_auc_score, auc
 
 
 def train_data(features, labels):
@@ -14,6 +18,29 @@ def test_data(clf, features):
 
 def get_accuracy(true_labels, predicted_labels):
     return metrics.accuracy_score(true_labels, predicted_labels)
+
+def evaluation(true_labels, predicted_labels):
+    accuracy = get_accuracy(true_labels,predicted_labels)
+    print(f'Model accuracy: {round(accuracy*100, 1)}%')
+    print("**********************************************")
+    print(metrics.confusion_matrix(true_labels, predicted_labels))
+    sns.heatmap(metrics.confusion_matrix(true_labels, predicted_labels), annot = True, fmt = ".0f", cmap = "YlGnBu")
+    plt.title("confusion matrix")
+    plt.show()
+    
+# def auc_plot(test_labels,predictions):
+#     train_fpr, train_tpr, tr_thresholds = roc_curve(test_labels, predictions)
+#     test_fpr, test_tpr, te_thresholds = roc_curve(test_labels, predictions)
+#     plt.grid()
+#     plt.plot(train_fpr, train_tpr, label=" AUC TRAIN ="+str(auc(train_fpr, train_tpr)))
+#     plt.plot(test_fpr, test_tpr, label=" AUC TEST ="+str(auc(test_fpr, test_tpr)))
+#     plt.plot([0,1],[0,1],'g--')
+#     plt.legend()
+#     plt.xlabel("True Positive Rate")
+#     plt.ylabel("False Positive Rate")
+#     plt.title("AUC(ROC curve)")
+#     plt.grid(color='black', linestyle='-', linewidth=0.5)
+#     plt.show()
 
 
 """
