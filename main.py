e17b1ef81c1d882c0726243106d4e722893e5c80
Montague <6136789+MontagueM@users.noreply.github.com>
2021-03-15 16:43:41 +0000

    Added Model class and tidied up code

--- main.py
+++ main.py
@@ -8,32 +8,32 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     feature_names, features, labels = data_handler.get_data()
 
-    #Uncomment if visualising data
-    #features = [x[:2] for x in features]
-    #feature_names = feature_names[:2]
-
     training_features, training_labels, test_features, test_labels = data_handler.split_data(features, labels)
 
-    svm_clf = model.train_data(training_features, training_labels)
-    predictions = model.test_data(svm_clf, test_features)
-    accuracy = model.get_accuracy(test_labels, predictions)
+    svm_model = model.Model(training_features, training_labels, test_features, test_labels)
+    svm_model.train()
+    svm_model.test()
 
-    print(f'Model accuracy: {round(accuracy*100, 1)}%')
+    # confusion matrix and accuracy
+    svm_model.evaluate()
 
-    #uncomment for confusion matrix and accuracy 
-    #model.evaluation(test_labels, predictions)
-
-
-    #uncomment for ROC curve and AUC score
-    #svc_disp = plot_roc_curve(svm_clf, test_features, test_labels)
-    #plt.show()
+    # ROC curve
+    svc_disp = plot_roc_curve(svm_model.clf, svm_model.test_features, svm_model.test_labels)
+    plt.show()
    
-    #uncomment to get 5 cross validation scores
-    #scores = data_handler.k_fold_cross_validation(k=5, kernel= 'linear',features=features,labels=labels)
-    #print(scores)
+    # Five-fold cross validation scores
+    scores = data_handler.k_fold_cross_validation(k=5, kernel='linear', features=features, labels=labels)
+    print(scores)
 
+    # AUC plot
+    svm_model.plot_auc()
 
-    #model.auc_plot(test_features, test_labels)
+    # 2D scatter plot
+    features = [x[:2] for x in features]
+    feature_names = feature_names[:2]
 
-    #uncomment for 2D scatter plot
-    #graph.visualise_clf(np.array(features), np.array(labels), svm_clf, feature_names)
+    training_features, training_labels, test_features, test_labels = data_handler.split_data(features, labels)
+    svm_model = model.Model(training_features, training_labels, test_features, test_labels)
+    svm_model.train()
+
+    graph.visualise_clf(np.array(features), np.array(labels), svm_model.clf, feature_names)
--- model.py
+++ model.py
@@ -5,46 +5,53 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, roc_auc_score, auc
 
 
-def train_data(features, labels):
-    clf = svm.SVC(kernel='linear')
-    clf.fit(features, labels)
-    return clf
+class Model:
+    def __init__(self, train_features, train_labels, test_features, test_labels):
+        self.train_features = train_features
+        self.train_labels = train_labels
+        self.test_features = test_features
+        self.test_labels = test_labels
 
+        self.clf = None
+        self.predicted_labels = None
 
-def test_data(clf, features):
-    predictions = clf.predict(features)
-    return predictions
+    def train(self):
+        self.clf = svm.SVC(kernel='linear')
+        self.clf.fit(self.train_features, self.train_labels)
+        return self.clf
 
+    def test(self):
+        self.predicted_labels = self.clf.predict(self.test_features)
+        return self.predicted_labels
 
-def get_accuracy(true_labels, predicted_labels):
-    return metrics.accuracy_score(true_labels, predicted_labels)
+    def get_accuracy(self):
+        return metrics.accuracy_score(self.test_labels, self.predicted_labels)
 
-def evaluation(true_labels, predicted_labels):
-    accuracy = get_accuracy(true_labels,predicted_labels)
-    print(f'Model accuracy: {round(accuracy*100, 1)}%')
-    print("**********************************************")
-    print(metrics.confusion_matrix(true_labels, predicted_labels))
-    sns.heatmap(metrics.confusion_matrix(true_labels, predicted_labels), annot = True, fmt = ".0f", cmap = "YlGnBu")
-    plt.title("confusion matrix")
-    plt.show()
-    
-# def auc_plot(test_labels,predictions):
-#     train_fpr, train_tpr, tr_thresholds = roc_curve(test_labels, predictions)
-#     test_fpr, test_tpr, te_thresholds = roc_curve(test_labels, predictions)
-#     plt.grid()
-#     plt.plot(train_fpr, train_tpr, label=" AUC TRAIN ="+str(auc(train_fpr, train_tpr)))
-#     plt.plot(test_fpr, test_tpr, label=" AUC TEST ="+str(auc(test_fpr, test_tpr)))
-#     plt.plot([0,1],[0,1],'g--')
-#     plt.legend()
-#     plt.xlabel("True Positive Rate")
-#     plt.ylabel("False Positive Rate")
-#     plt.title("AUC(ROC curve)")
-#     plt.grid(color='black', linestyle='-', linewidth=0.5)
-#     plt.show()
+    def evaluate(self, b_confusion_matrix=True):
+        if self.predicted_labels is None:
+            self.test()
+        accuracy = self.get_accuracy()
+        print(f'Model accuracy: {round(accuracy * 100, 1)}%')
+        print("**********************************************")
+        if b_confusion_matrix:
+            self.plot_confusion_matrix()
 
+    def plot_confusion_matrix(self):
+        print(metrics.confusion_matrix(self.test_labels, self.predicted_labels))
+        sns.heatmap(metrics.confusion_matrix(self.test_labels, self.predicted_labels), annot=True, fmt=".0f", cmap="YlGnBu")
+        plt.title("confusion matrix")
+        plt.show()
 
-"""
-Things to look into:
-- ROC curve (prediction vs random chance)
-- p-hacking (same data for multiple hypothesis)
-"""
+    def plot_auc(self):
+        train_fpr, train_tpr, tr_thresholds = roc_curve(self.test_labels, self.predicted_labels)
+        test_fpr, test_tpr, te_thresholds = roc_curve(self.test_labels, self.predicted_labels)
+        plt.grid()
+        plt.plot(train_fpr, train_tpr, label=" AUC TRAIN ="+str(auc(train_fpr, train_tpr)))
+        plt.plot(test_fpr, test_tpr, label=" AUC TEST ="+str(auc(test_fpr, test_tpr)))
+        plt.plot([0,1],[0,1],'g--')
+        plt.legend()
+        plt.xlabel("True Positive Rate")
+        plt.ylabel("False Positive Rate")
+        plt.title("AUC(ROC curve)")
+        plt.grid(color='black', linestyle='-', linewidth=0.5)
+        plt.show()
